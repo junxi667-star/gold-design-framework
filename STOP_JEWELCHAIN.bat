5611dd2f1e35f5ef39819bd5a22420fd04e2eaae
im10furry <xiaoliuwj.china@gmail.com>
2026-08-09 04:08:30 +0800

    refactor: remove legacy documentation and scripts
    - Remove legacy docs (AGENT_STORAGE_HANDOFF, CLOUDFLARE_PAGES_DEPLOY, etc.)
    - Remove legacy test report and UI upgrade notes
    - Remove legacy batch files (STOP_IMAGE_WORKER, STOP_JEWELCHAIN)
    - Remove legacy Node.js scripts (check-frontend-assets, diagnose, etc.)
    - Remove legacy Windows service managers (service-manager.js, worker-service-manager.js)

--- STOP_JEWELCHAIN.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal
-call "%~dp0scripts\windows\lifecycle\stop-jewelchain.bat" %*
-exit /b %errorlevel%
--- scripts/sync-frontend-assets.js
+++
@@ -1,4 +0,0 @@
-import { sharedFrontendAssets, syncFrontendAssets } from "./frontend-assets.js";
-
-await syncFrontendAssets();
-console.log(`Synced ${sharedFrontendAssets.length} shared frontend assets to pages-frontend.`);
